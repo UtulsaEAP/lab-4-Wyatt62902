1b904c70a4930513a44510c186020833e521066d
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-02-15 13:54:26 -0600

    problem 2

--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -8,7 +8,21 @@ Lab Time:
 def password_mod():
     word = input()
     password = ''
-    # Type your code here.
+    for letter in word:
+        if letter == "i":
+            password = password+"1"
+        if letter == "a":
+            password = password+"@"
+        if letter == "m":
+            password = password+"M"
+        if letter == "B":
+            password = password+"8"
+        if letter == "s":
+            password = password+"$"
+        else:
+            password = password+str(letter)
+    password = password+"!"
+    print(password)
 
 if __name__ == "__main__":
     password_mod()
